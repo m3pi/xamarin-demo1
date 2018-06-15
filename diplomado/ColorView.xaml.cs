738b800d26d6a86ae435594dfbf40d83f80ffb8a
Oscar Saavedra <oscar.saavedra@idslatam.com>
2018-06-15 17:45:38 -0500

    8-5 Vistas personalizadas basadas en XAML
    Hay varias maneras de evitar la repetición del marcado en XAML. Una técnica común consiste en crear una nueva clase basada en XAML que se deriva de ContentView

---
+++ diplomado/ColorView.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace diplomado
+{
+    public partial class ColorView : ContentView
+    {
+        string colorName;
+        ColorTypeConverter colorTypeConv = new ColorTypeConverter();
+
+        public ColorView()
+        {
+            InitializeComponent();
+        }
+
+        public string ColorName
+        {
+            get
+            {
+                return colorName;
+            }
+
+            set{
+                // Set the name
+                colorName = value;
+                colorNameLabel.Text = value;
+
+                //Get the actual Color and set the other views
+                Color color = (Color)colorTypeConv.ConvertFromInvariantString(colorName);
+
+                boxView.Color = color;
+                colorValueLabel.Text = string.Format("{0:X2}-{1:X2}-{2:X2}",
+                                                     (int)(255 * color.R),
+                                                     (int)(255 * color.G),
+                                                     (int)(255 * color.B));
+            }
+        }
+    }
+
+}
