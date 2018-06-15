53e35d3f55c51e7ca8df9843402ffd37d38edc98
Oscar Saavedra <oscar.saavedra@idslatam.com>
2018-06-15 16:33:15 -0500

    Cap 8-1 cómo utilizar la x:Arguments etiqueta para especificar los argumentos de constructor

--- diplomado/MainPage.xaml.cs
+++ diplomado/MainPage.xaml.cs
@@ -14,21 +14,6 @@ namespace diplomado
         public MainPage()
         {
             InitializeComponent();
-
-            Label label = new Label
-            {
-                Text = "Hello from Code!",
-                IsVisible = true,
-                Opacity = 0.75,
-                HorizontalTextAlignment = TextAlignment.Center,
-                VerticalOptions = LayoutOptions.CenterAndExpand,
-                TextColor = Color.Blue,
-                BackgroundColor = Color.FromRgb(255, 128, 128),
-                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
-                FontAttributes = FontAttributes.Bold | FontAttributes.Italic
-            };
-
-            (Content as StackLayout).Children.Insert(0, label);
         }
 
     }
