3ab06aaceadea42870d0db2a975957bc1350da2e
Oscar Saavedra <oscar.saavedra@idslatam.com>
2018-06-15 11:53:53 -0500

    Cap 5

--- diplomado/MainPage.xaml.cs
+++ diplomado/MainPage.xaml.cs
@@ -11,234 +11,72 @@ namespace diplomado
 {
     public partial class MainPage : ContentPage
     {
+        //Label label;
         public MainPage()
         {
-            //Color[] colors = { Color.Yellow, Color.Blue };
-            //int flipFlopper = 0;
 
-            //// Create Labels sorted by LayoutAlignment property.
-            //IEnumerable<Label> labels =
-            //    from field in typeof(LayoutOptions).GetRuntimeFields()
-            //    where field.IsPublic && field.IsStatic
-            //    orderby ((LayoutOptions)field.GetValue(null)).Alignment
-            //    select new Label
-            //    {
-            //        Text = "VerticalOptions = " + field.Name,
-            //        VerticalOptions = (LayoutOptions)field.GetValue(null),
-            //        HorizontalTextAlignment = TextAlignment.Center,
-            //        FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
-            //        TextColor = colors[flipFlopper],
-            //        BackgroundColor = colors[flipFlopper = 1 - flipFlopper]
-            //    };
-
-            //// Transfer to StackLayout.
-            //StackLayout stackLayout = new StackLayout();
-
-            //foreach (Label label in labels)
+            //ejemplo utiliza este evento para mostrar el tamaño de la pantalla del programa.
+            //label = new Label
             //{
-            //    stackLayout.Children.Add(label);
-            //}
-
-            //Padding = new Thickness(0, Device.RuntimePlatform == Device.iOS ? 20 : 0, 0, 0);
-            //Content = stackLayout;
-
-
-            //BackgroundColor = Color.Aqua;
-
-            //Content = new Frame
-            //{
-            //    OutlineColor = Color.Black,
-            //    BackgroundColor = Color.Yellow,
+            //    FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
             //    HorizontalOptions = LayoutOptions.Center,
-            //    VerticalOptions = LayoutOptions.Center,
-
-            //    Content = new Label
-            //    {
-            //        Text = "I've been framed!",
-            //        FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
-            //        FontAttributes = FontAttributes.Italic,
-            //        TextColor = Color.Blue
-            //    }
+            //    VerticalOptions = LayoutOptions.Center
             //};
 
+            //Content = label;
 
-            //BackgroundColor = Color.Pink;
+            //SizeChanged += OnPageSizeChanged;
 
+
+            //para mostrar una BoxView una pulgada de alto y uno centímetros amplia.
             //Content = new BoxView
             //{
-            //    Color = Color.Navy,
+            //    Color = Color.Accent,
+            //    WidthRequest = 64,
+            //    HeightRequest = 160,
             //    HorizontalOptions = LayoutOptions.Center,
-            //    VerticalOptions = LayoutOptions.Center,
-            //    WidthRequest = 200,
-            //    HeightRequest = 100
+            //    VerticalOptions = LayoutOptions.Center
             //};
 
 
+            //Ajustar el texto al tamaño disponible
+            // https://docs.microsoft.com/es-es/xamarin/xamarin-forms/creating-mobile-apps-xamarin-forms/summaries/chapter05
 
-        //    StackLayout stackLayout = new StackLayout();
-
-        //    // Loop through the Color structure fields.
-        //    foreach (FieldInfo info in typeof(Color).GetRuntimeFields())
-        //    {
-        //        // Skip the obsolete (i.e. misspelled) colors.
-        //        if (info.GetCustomAttribute<ObsoleteAttribute>() != null)
-        //            continue;
-
-        //        if (info.IsPublic &&
-        //            info.IsStatic &&
-        //            info.FieldType == typeof(Color))
-        //        {
-        //            stackLayout.Children.Add(
-        //                CreateColorView((Color)info.GetValue(null), info.Name));
-        //        }
-        //    }
-
-        //    // Loop through the Color structure properties.
-        //    foreach (PropertyInfo info in typeof(Color).GetRuntimeProperties())
-        //    {
-        //        MethodInfo methodInfo = info.GetMethod;
-
-        //        if (methodInfo.IsPublic &&
-        //            methodInfo.IsStatic &&
-        //            methodInfo.ReturnType == typeof(Color))
-        //        {
-        //            stackLayout.Children.Add(
-        //                CreateColorView((Color)info.GetValue(null), info.Name));
-        //        }
-        //    }
-
-        //    Padding = new Thickness(5, Device.RuntimePlatform == Device.iOS ? 20 : 5, 5, 5);
-
-        //    // Put the StackLayout in a ScrollView.
-        //    Content = new ScrollView
-        //    {
-        //        Content = stackLayout,
-        //        HorizontalOptions = LayoutOptions.Center
-        //    };
-        //}
-
-        //View CreateColorView(Color color, string name)
-        //{
-            //return new Frame
-            //{
-            //    OutlineColor = Color.Accent,
-            //    Padding = new Thickness(5),
-            //    Content = new StackLayout
-            //    {
-            //        Orientation = StackOrientation.Horizontal,
-            //        Spacing = 15,
-            //        Children =
-            //        {
-            //            new BoxView
-            //            {
-            //                Color = color
-            //            },
-            //            new Label
-            //            {
-            //                Text = name,
-            //                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
-            //                FontAttributes = FontAttributes.Bold,
-            //                VerticalOptions = LayoutOptions.Center,
-            //                HorizontalOptions = LayoutOptions.StartAndExpand
-            //            },
-            //            new StackLayout
-            //            {
-            //                Children =
-            //                {
-            //                    new Label
-            //                    {
-            //                        Text = String.Format("{0:X2}-{1:X2}-{2:X2}",
-            //                                             (int)(255 * color.R),
-            //                                             (int)(255 * color.G),
-            //                                             (int)(255 * color.B)),
-            //                        VerticalOptions = LayoutOptions.CenterAndExpand
-            //                    },
-            //                    new Label
-            //                    {
-            //                        Text = String.Format("{0:F2}, {1:F2}, {2:F2}",
-            //                                             color.Hue,
-            //                                             color.Saturation,
-            //                                             color.Luminosity),
-            //                        VerticalOptions = LayoutOptions.CenterAndExpand
-            //                    }
-            //                },
-            //                IsVisible = color != Color.Default,
-            //                HorizontalOptions = LayoutOptions.End
-            //            }
-            //        }
-            //    }
-            //};
-
-
-            StackLayout mainStack = new StackLayout();
-            StackLayout textStack = new StackLayout
+            // Device.StartTimer para iniciar un temporizador que notifica periódicamente a la aplicación cuando llega el momento para actualizar el reloj
+            Label clockLabel = new Label
             {
-                Padding = new Thickness(5),
-                Spacing = 10
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center
             };
 
-            // Get access to the text resource.
-            Assembly assembly = GetType().GetTypeInfo().Assembly;
-            string resource = "BlackCat.Texts.TheBlackCat.txt";
+            Content = clockLabel;
 
-            using (Stream stream = assembly.GetManifestResourceStream(resource))
+            // Handle the SizeChanged event for the page.
+            SizeChanged += (object sender, EventArgs args) =>
             {
-                using (StreamReader reader = new StreamReader(stream))
-                {
-                    bool gotTitle = false;
-                    string line;
-
-                    // Read in a line (which is actually a paragraph).
-                    while (null != (line = reader.ReadLine()))
-                    {
-                        Label label = new Label
-                        {
-                            Text = line,
-
-                            // Black text for ebooks!
-                            TextColor = Color.Black
-                        };
-
-                        if (!gotTitle)
-                        {
-                            // Add first label (the title) to mainStack.
-                            label.HorizontalOptions = LayoutOptions.Center;
-                            label.FontSize = Device.GetNamedSize(NamedSize.Medium, label);
-                            label.FontAttributes = FontAttributes.Bold;
-                            mainStack.Children.Add(label);
-                            gotTitle = true;
-                        }
-                        else
-                        {
-                            // Add subsequent labels to textStack.
-                            textStack.Children.Add(label);
-                        }
-                    }
-                }
-            }
-
-            // Put the textStack in a ScrollView with FillAndExpand.
-            ScrollView scrollView = new ScrollView
-            {
-                Content = textStack,
-                VerticalOptions = LayoutOptions.FillAndExpand,
-                Padding = new Thickness(5, 0),
+                // Scale the font size to the page width
+                //      (based on 11 characters in the displayed string).
+                if (this.Width > 0)
+                    clockLabel.FontSize = this.Width / 6;
             };
 
-            // Add the ScrollView as a second child of mainStack.
-            mainStack.Children.Add(scrollView);
+            // Start the timer going.
+            Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+            {
+                // Set the Text property of the Label.
+                clockLabel.Text = DateTime.Now.ToString("h:mm:ss tt");
+                return true;
+            });
 
-            // Set page content to mainStack.
-            Content = mainStack;
 
-            // White background for ebooks!
-            BackgroundColor = Color.White;
-
-            // Add some iOS padding for the page
-            Padding = new Thickness(0, Device.RuntimePlatform == Device.iOS ? 20 : 0, 0, 0);
 
             //InitializeComponent();
         }
+
+        //void OnPageSizeChanged(object sender, EventArgs args)
+        //{
+        //    label.Text = String.Format("{0} \u00D7 {1}", Width, Height);
+        //}
 
     }
 }
