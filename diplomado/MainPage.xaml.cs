6cc28f5295d7cc7288c44bb3dcfa19c17cd2938e
Oscar Saavedra <oscar.saavedra@idslatam.com>
2018-06-15 18:11:19 -0500

    Cap 8-6 Eventos y controladores Eventos se pueden asignar a los controladores de eventos en XAML, pero se debe implementar su propio controlador de eventos en el archivo de código subyacente

--- diplomado/MainPage.xaml.cs
+++ diplomado/MainPage.xaml.cs
@@ -11,9 +11,36 @@ namespace diplomado
 {
     public partial class MainPage : ContentPage
     {
+        App app = Application.Current as App;
+        
         public MainPage()
         {
             InitializeComponent();
+
+            displayLabel.Text = app.DisplayLabelText;
+            backspaceButton.IsEnabled = displayLabel.Text != null &&
+                displayLabel.Text.Length > 0;
+
+        }
+
+        void OnDigitButtonClicked(object sender, EventArgs args)
+        {
+            Button button = (Button)sender;
+
+            displayLabel.Text += (string)button.StyleId;
+            backspaceButton.IsEnabled = true;
+
+            app.DisplayLabelText = displayLabel.Text;
+        }
+
+        void OnBackspaceButtonClicked(object sender, EventArgs args)
+        {
+            string text = displayLabel.Text;
+
+            displayLabel.Text = text.Substring(0, text.Length - 1);
+            backspaceButton.IsEnabled = displayLabel.Text.Length > 0;
+
+            app.DisplayLabelText = displayLabel.Text;
         }
 
     }
