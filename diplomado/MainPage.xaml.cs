b5b90b22b1456d8b22f03ee60ce88e5a4c401449
Oscar Saavedra <oscar.saavedra@idslatam.com>
2018-06-18 10:55:54 -0500

    19 El selector y el control de eventos
    cómo utilizar XAML para establecer el Picker Title propiedad y agregue string elementos a la Items colección. Cuando el usuario selecciona el Picker, muestra los elementos en el Items colección de una manera depende de la plataforma.

--- diplomado/MainPage.xaml.cs
+++ diplomado/MainPage.xaml.cs
@@ -17,6 +17,22 @@ namespace diplomado
             InitializeComponent();
         }
 
+        void OnPickerSelectedIndexChanged(object sender, EventArgs args)
+        {
+            if (entry == null)
+                return;
+
+            Picker picker = (Picker)sender;
+            int selectedIndex = picker.SelectedIndex;
+
+            if (selectedIndex == -1)
+                return;
+
+            string selectedItem = picker.Items[selectedIndex];
+            PropertyInfo propertyInfo = typeof(Keyboard).GetRuntimeProperty(selectedItem);
+            entry.Keyboard = (Keyboard)propertyInfo.GetValue(null);
+        }
+
 
     }
 }
